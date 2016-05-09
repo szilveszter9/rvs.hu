e39b57d6fc6e29dcff6c70505c01968b50f3e42c
Szilveszter Szilagyi <szilveszter@gmx.co.uk>
2016-05-09 06:54:48 +0100

    Card page with keyboardlist, reading keyboards form json

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,13 +1,54 @@
 'use strict';
 import React, { Component } from 'react';
 import { render } from 'react-dom';
+import { getJSON } from './lib/http.js';
+
+class KeyboardList extends Component {
+  render() {
+    console.log(this.props.data);
+    var keyboards = this.props.data.map(keyboard => {
+      return (
+        <div key={keyboard.id}>
+          <img src={keyboard.imageUrl}/>
+        </div>
+      );
+    });
+
+    return (
+      <div className="keyboard--item">
+        <h2>Keyboards</h2>
+        {keyboards}
+      </div>
+    );
+  }
+}
 
 export default class Card extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {data: []};
+  }
+
+  componentDidMount() {
+    this.getKeyboards();
+  }
+
+  getKeyboards() {
+    getJSON(
+      'data/keyboardList.json',
+      (err, data) => {
+        if(err) /*if(cnt++ < 5) tryAgain*/;
+        else this.setState({ data });
+      }
+    );
+  }
+
   render() {
     console.log('card render');
     return (
       <div>
         <h1>Card</h1>
+        <KeyboardList data={this.state.data}/>
       </div>
     );
   }
---
+++ src/lib/http.js
@@ -0,0 +1,17 @@
+'use strict';
+
+export function get (url, cb) {
+  var req = new XMLHttpRequest();
+  req.onload = () =>
+    req.status === 404 ?
+      cb(new Error('not found')) :
+      cb(null, req.response);
+  req.open('GET', url);
+  req.send();
+}
+
+export function getJSON (url, cb) {
+  get(url, (err, response) => {
+    cb(err, JSON.parse(response))
+  });
+}
